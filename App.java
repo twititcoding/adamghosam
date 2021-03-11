4204438c70849c6f56623ac79f5cc77e9910cd02
twititcoding <76032103+twititcoding@users.noreply.github.com>
2021-03-11 23:29:41 +0700

    Add files via upload
    program sederhana mengunakan java tugas pemrograman pbo

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+ 	int a, b, c, d;
+        a = -5 + 8 * 6;
+        b = (55 + 9) % 9;
+        c = 20 + -3 * 5 / 8;
+        d = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(a);
+	System.out.println(b);
+  	System.out.println(c);
+ 	System.out.println(d);
+
+    }
+}
